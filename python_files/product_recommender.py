f8d6d33e234328b83baa8dbdf73cd89dec889570
Mindy Zhou <minzhou16@gmail.com>
2019-09-11 12:10:19 -0400

    add no items detected scenario

--- python_files/product_recommender.py
+++ python_files/product_recommender.py
@@ -205,16 +205,21 @@ class ProductRecommender:
                     highest_image[2] = file
                     highest_score[2] = pred_c[2]
         # recommend products based upon most percentage per total num of pics
-        # make sure total is not zero and not just one image
+        # make sure total is not zero
         if total:
-            if len(files) == 1:
+            # there are no items identified
+            if hat or beard or eyewear:
+                title = 'No items detected for this user!'
+            # just one image, pick max score
+            elif len(files) == 1:
                 index = np.argmax(pred_c)
             else:
                 index = np.argmax([hat/total, beard/total, eyewear/total])
             product = ['hat', 'beard', 'eyewear'][index]
             img_chosen = highest_image[index]
             self.plot_image(img_chosen)
-            plt.title(f"We recommend {product} products to this IG user!")
+            title = f"We recommend {product} products to this IG user!"
+            plt.title(title)
             plt.show(block=False)
             plt.pause(3)
             plt.close()
